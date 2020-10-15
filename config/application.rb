6ac934e2ceecd7053604c049f620032e501e5618
junyoshimura57 <jun.yoshimura57@gmail.com>
2020-10-15 13:48:13 +0900

    :sparkles: Add generate config
    generateコマンド時に生成されるファイルを制限

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,16 @@ module InstaClone
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # generatorsコマンド時の自動生成を防ぎたいファイルを以下に記載
+    config.generators do |g|
+      # css,jsのファイルは作成せず
+      g.assets false
+      # コントローラー作成時にconfig/routes.rbが書き換えられないようにする。
+      g.skip_routes false
+      # テストファイルは作成せず
+      g.test_framework false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
