460454abb432a9ff9a663f7ed69e7655089d18e2
junyoshimura57 <jun.yoshimura57@gmail.com>
2020-12-20 12:22:30 +0900

    :construction: Add method user.rb
    ユーザーモデルにメソッドを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,13 +44,13 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
   # ■followedテーブル(usersテーブル)との関連を定義(active_relationships経緯)
   # 中間テーブルを介してfollowedモデルのUser(フォローされた側)を集めることを「following」と定義する。(source: :userとやりたくなるが、user_idと内部結合使用としてしまうためNG)
-  has_many :following, throgh :active_relationships, source: :followed
+  has_many :following, through: :active_relationships, source: :followed
 
   # 【特定のユーザーがフォローされるユーザーとの関係】
   # ■relationshipsテーブル(中間テーブル)との関連を定義
   has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
   # ■followerテーブル(usersテーブル)との関連を定義(passive_relationships経緯)
-  has_many :followers, throgh :passive_relationships, source: :follower
+  has_many :followers, through: :passive_relationships, source: :follower
 
   # 投稿が自身のものかを判別するためのメソッドを定義
   def own?(object)
@@ -75,4 +75,32 @@ class User < ApplicationRecord
   def like?(post)
     like_posts.include?(post)
   end
+
+  # ①self.followingでcurrent_userがフォローしたユーザーを取得。
+  # ②引数のユーザーを①に追加
+  def follow(other_user)
+    following << other_user
+  end
+  
+  # ①self.followingでcurrent_userがフォローしたユーザーを取得。
+  # ②destroyメソッドをメソッドチェーンして、引数のユーザーを削除。
+  def unfollow(other_user)
+    following.destroy(other_user)
+  end
+
+  # ①self.followingでcurrent_userがフォローしたユーザーを取得。
+  # ②include?メソッドをメソッドチェーンして、引数のユーザーが含まれているか確認。(フォローしているか確認できる)
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
+  # ①「self.following_ids << self.id」でcurrent_userがフォローしているユーザーidにcurrent_userのユーザーidを追加する。
+  # ②postテーブルで上記①のユーザーidの投稿をActiveRecord::Relationで返す。(自分と投稿とフォローしているユーザーの投稿を取得できる)
+  def feed
+    Post.where(user_id: following_ids << id)
+  end
+
+  # scopeでrecentメソッド()を定義する。
+  # クラスメソッドと異なり常にActiveRecord::Relationオブジェクトを返すためメソッドチェーンを使える。(nilの場合は、allメソッドとなる)
+  scope :recent, ->(count) { order(created_at: :desc).limit(count) }
 end
