fe5ba490d61b5a10043ccdfed2ad5720c6723ddb
junyoshimura57 <jun.yoshimura57@gmail.com>
2021-04-25 16:09:28 +0900

    :recycle: Be rubocop -a
    rubocop -aを実行

--- app/controllers/mypage/notification_settings_controller.rb
+++ app/controllers/mypage/notification_settings_controller.rb
@@ -6,7 +6,7 @@ class Mypage::NotificationSettingsController < Mypage::BaseController
   def update
     @user = User.find(current_user.id)
     if @user.update(notification_params)
-      #　更新が成功した場合には、フレッシュメッセージを渡して、通知編集画面にレンダリング。
+      # 　更新が成功した場合には、フレッシュメッセージを渡して、通知編集画面にリダイレクト。
       redirect_to edit_mypage_notification_setting_path, success: '通知設定を更新しました'
     else
       flash.now['danger'] = '通知設定の更新に失敗しました'
@@ -15,7 +15,7 @@ class Mypage::NotificationSettingsController < Mypage::BaseController
   end
 
   private
-  
+
   def notification_params
     params.require(:user).permit(:notification_like, :notification_comment, :notification_follow)
   end
